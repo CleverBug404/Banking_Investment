1eaae3faf8fed56163f828f6cf48b2c74439e386
JBlades10 <juan.velasquez@snhu.edu>
2022-12-03 17:57:22 -0500

    Add project files.

---
+++ dashboard.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+
+void displayTable() {
+	cout << endl << "Balance and Interest Without Additional Monthly Deposits" << endl;
+	cout << "================================================================" << endl;
+	cout << "Year          Year End Balance          Year End Earned Interest" << endl;
+	cout << "----------------------------------------------------------------" << endl;
+}
